70f2b47806e05a8495d5cd5a49a03ffd6fcfb834
Osondu Samuel <samuelosondu99@gmail.com>
2023-05-07 01:32:42 +0100

    1st Commit

---
+++ blog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, "blog/index.html")
+
+
+def blog(request):
+    return render(request, "blog/blog.html")
+
+
+def post(request):
+    return render(request, "blog/post.html")
